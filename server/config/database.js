6944e5dd3afd1b8077539e561001d6d9fd962a96
Tony Klinpon <thitiklinpon@gmail.com>
2024-04-14 16:35:03 -0400

    Revert "Hard coding DB URL"
    This reverts commit 616e2ef8a6934575f1a2bb21567dcd45de55a13d.

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  databaseUrl:
-    process.env.DB_URL ||
-    "mongodb+srv://thitiklinpon:kvdkRIapo4eYrTmV@cluster0.vxj9lmm.mongodb.net/amazon",
+  databaseUrl: process.env.DB_URL,
   secret: "mysecretkey",
 };
