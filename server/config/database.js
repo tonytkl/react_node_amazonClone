4ecf010db96198e31ca81e91049185290d748afc
Tony Klinpon <thitiklinpon@gmail.com>
2024-04-15 09:51:30 -0400

    Remove env variable

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   databaseUrl:
-    process.env.DB_URL ||
     "mongodb+srv://thitiklinpon:kvdkRIapo4eYrTmV@cluster0.vxj9lmm.mongodb.net/amazon",
   secret: "mysecretkey",
 };
