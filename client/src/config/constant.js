721974cb68d39b32dddf4bb927d16edd992e54af
Tony Klinpon <78643158+tonytkl@users.noreply.github.com>
2024-06-18 13:39:12 -0400

    Update constant.js
    Change end point of backend

--- client/src/config/constant.js
+++ client/src/config/constant.js
@@ -1,3 +1,3 @@
-// export const endPoint = "https://react-node-amazon-clone.vercel.app";
-export const endPoint = "http://localhost:8000";
+export const endPoint = "https://react-node-amazon-clone.vercel.app";
+// export const endPoint = "http://localhost:8000";
 export const totitleCase = (str) => str[0].toUpperCase() + str.slice(1);
